5bdf078357cbd6442a212c6bc75af674febf8329
Ayush <srihari.ayush@gmail.com>
2017-10-10 12:44:36 +0530

    test for increase in score and removing user inputted letters

--- test_letter_hunt.py
+++ test_letter_hunt.py
@@ -2,6 +2,22 @@ import letter_hunt
 
 
 def test_random_letter_generator():
-    letter = random_choice() in active_letter
+    letter = letter_hunt.random_choice() in letter_hunt.letters
     assert letter == True
 
+def test_score_increse():
+    letter_hunt.letters=['a','b','c']
+    letter_hunt.active_letters=['a','b']
+    letter_hunt.letter_check('b')
+    assert letter_hunt.score == 1
+    assert letter_hunt.fail == 0
+    assert letter_hunt.active_letters == ['a']
+
+
+def test_fail_increse():
+    letter_hunt.letters=['a','b','c']
+    letter_hunt.active_letters=['a','b']
+    letter_hunt.letter_check('c')
+    #assert letter_hunt.score == 1
+    assert letter_hunt.fail == 1
+    assert letter_hunt.active_letters == ['a','b']
