0bd82077b907b2112567d2fda8e830f400f25f05
Ayush <srihari.ayush@gmail.com>
2017-10-10 12:43:37 +0530

    checking user input and increasing score, deleting if input is present in active letters

--- letter_hunt.py
+++ letter_hunt.py
@@ -16,19 +16,23 @@ def active_add_letters():
     random_letter=random_choice()
     time.sleep(sleep_time)
     active_letters.append(random_letter)
+    print(active_letters)
     return active_letters
 
 def letter_check(user_input):
     global score
     global fail
-    active_add_letters()
+   # active_add_letters()
    # score=0
     #fail=0
     if user_input in active_letters:
+        active_letters.remove(user_input)
         score += 1
     else:
         fail += 1
 
     print("score is {}".format(score))
     print("{} failed attempts".format(fail))
+    print(active_letters)
+   # return score,fail
     
