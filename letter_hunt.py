a002944745a24b4f386eab55639666a6a5644cf4
Ligin Vellakkad <ligindd@gmail.com>
2017-10-17 05:40:08 +0530

    Format with pep8 style guide.

--- letter_hunt.py
+++ letter_hunt.py
@@ -1,4 +1,4 @@
-import datetime
+# import datetime # unused
 import time
 import random
 import string
@@ -9,17 +9,20 @@ letters = list(string.ascii_lowercase)
 score = 0
 fail = 0
 active_letters = []
-sleep_time=2
+sleep_time = 2
+
 
 def random_choice():
     return random.choice(letters)
 
+
 def active_add_letters():
-    random_letter=random_choice()
+    random_letter = random_choice()
     time.sleep(sleep_time)
     active_letters.append(random_letter)
     print(active_letters)
     return active_letters
+
 
 def count_strike(input):
     global strike_list
@@ -41,12 +44,13 @@ def count_strike(input):
     else:
         strike_list = [input]
 
+
 def letter_check(user_input):
     global score
     global fail
-   # active_add_letters()
-   # score=0
-    #fail=0
+    # active_add_letters()
+    # score=0
+    # fail=0
     if user_input in active_letters:
         active_letters.remove(user_input)
         score += 1
